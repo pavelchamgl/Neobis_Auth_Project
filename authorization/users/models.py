7d633ff53e11fa98058da8f0b285698a964a7467
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2023-06-02 20:32:35 +0300

    Create user model

--- authorization/users/models.py
+++ authorization/users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=50, blank=True, null=True)
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(unique=True)
+    birth_date = models.DateField(blank=True, null=True)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return f"{self.pk} - {self.email}"
+
+    class Meta:
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профили"
