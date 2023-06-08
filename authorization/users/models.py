acc34eba135d848a017bbd44e8be251e22eb1ad3
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2023-06-08 20:39:36 +0300

    Added params phone_number for user model

--- authorization/users/models.py
+++ authorization/users/models.py
@@ -10,6 +10,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=100, blank=True, null=True)
     email = models.EmailField(unique=True)
     birth_date = models.DateField(blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
     is_verified = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
